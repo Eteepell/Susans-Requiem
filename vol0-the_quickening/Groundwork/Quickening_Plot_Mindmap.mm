66b8fa7ee60772e634f9c0416dc6b1eed780a990
eteepell <eric.teepell@gmail.com>
2018-01-23 22:41:23 -0500

    .

---
+++ vol0-the_quickening/Groundwork/Quickening_Plot_Mindmap.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1516762872004" ID="ID_102257005" MODIFIED="1516763567251" TEXT="vol0-the_quickening [Susan&apos;s Requiem]"/>
+</map>
